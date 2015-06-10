358815f52b69a15b6a4eb1d8315afe2a5fae9643
Deluan <deluan@thoughtworks.com>
2015-06-10 18:34:55 -0400

    Adding "temporary" Solr 5.2 formula (until the official one is updated)

---
+++ solr52.rb
@@ -0,0 +1,49 @@
+class Solr52 < Formula
+  desc "Enterprise search platform from the Apache Lucene project"
+  homepage "https://lucene.apache.org/solr/"
+  url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/5.2.0/solr-5.2.0.tgz"
+  mirror "https://archive.apache.org/dist/lucene/solr/5.2.0/solr-5.2.0.tgz"
+  sha256 "dff2c5df505b732e23178e8f12e817dba4a088a8afe6599dcacf9ea241f76a1e"
+
+  depends_on :java
+
+  skip_clean "example/logs"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install "#{libexec}/bin/solr"
+    share.install "#{libexec}/bin/solr.in.sh"
+    prefix.install "#{libexec}/example"
+    prefix.install "#{libexec}/server"
+  end
+
+  plist_options :manual => "solr start"
+
+  def plist; <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/solr</string>
+            <string>start</string>
+            <string>-f</string>
+          </array>
+          <key>ServiceDescription</key>
+          <string>#{name}</string>
+          <key>WorkingDirectory</key>
+          <string>#{HOMEBREW_PREFIX}</string>
+          <key>RunAtLoad</key>
+          <true/>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system "solr"
+  end
+end
