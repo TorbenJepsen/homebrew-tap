4043693f06beb0ac26b6c45b8a308c87e70477cd
Deluan <deluan@thoughtworks.com>
2015-06-10 18:34:04 -0400

    More doco

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 brew-tap
 ========
 
-My personal homebrew tap. To use, run `brew tap deluan/tap`.
+My personal [Homebrew](http://brew.sh) tap. To use it, run: `brew tap deluan/tap`.
 
-Available formulas:
+Provided formulas:
 
 * LazyBones: https://github.com/pledbrook/lazybones
 * Grails.sh: https://github.com/deluan/grails.sh
--- grails.sh.rb
+++ grails.sh.rb
@@ -1,16 +1,18 @@
 class GrailsSh < Formula
   desc "Bash script for selecting and calling the correct Grails version when you have more than one version installed"
   homepage "https://github.com/deluan/grails.sh"
-  url "https://github.com/deluan/grails.sh/archive/b86a0781e91dc4b355bc1f2e16db10d2a3cd2ba7.zip"
   version "b86a0781"
+  url "https://github.com/deluan/grails.sh/archive/#{version}.zip"
   sha256 "3c666b0bc0013adcb0430347a4eed60a07e24ba6b2b7517fd3804d8e43528d30"
-  conflicts_with "grails"
+
+  conflicts_with "grails", :because => "Required versions of Grails will be downloaded on demand"
 
   def install
     bin.install 'grails'
   end
 
   def caveats; <<-EOS.undent
+    * Grails CANNOT be installed through Homebrew.
     * All your Grails versions must be installed under the same base directory. Ex:
         /opt/grails-1.0.3
         /opt/grails-1.1.1
