b5d83a9f5a89ae44401abab0bb256ea2560b8f70
Deluan <deluan@thoughtworks.com>
2015-12-02 16:05:04 -0500

    New grails.sh version

--- grails.sh.rb
+++ grails.sh.rb
@@ -1,9 +1,9 @@
 class GrailsSh < Formula
   desc "Bash script for selecting and calling the correct Grails version when you have more than one version installed"
   homepage "https://github.com/deluan/grails.sh"
-  version "b86a0781"
+  version "28ee603"
   url "https://github.com/deluan/grails.sh/archive/#{version}.zip"
-  sha256 "3c666b0bc0013adcb0430347a4eed60a07e24ba6b2b7517fd3804d8e43528d30"
+  sha256 "6bb021074ddd6ddb7b640691b1d6c5cd7269f3a601d6b947d54a66ed0be93623"
 
   conflicts_with "grails", :because => "Required versions of Grails will be downloaded on demand"
 
